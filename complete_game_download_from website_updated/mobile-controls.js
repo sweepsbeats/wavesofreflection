ecbb6b511d1f4be157396ac6eb01031b9ad5fb4f
sweepsbeats <60484729+sweepsbeats@users.noreply.github.com>
2025-04-17 17:44:51 -0700

    Update mobile-controls.js

--- complete_game_download_from website_updated/mobile-controls.js
+++ complete_game_download_from website_updated/mobile-controls.js
@@ -75,7 +75,7 @@ class MobileControls {
         this.flyButton.id = 'fly-button';
         this.flyButton.style.cssText = `
             position: fixed;
-            bottom: 90px;
+            bottom: 100px;
             left: 30px;
             width: 100px;
             height: 100px;
@@ -103,7 +103,7 @@ class MobileControls {
         this.joystickContainer.id = 'joystick-container';
         this.joystickContainer.style.cssText = `
             position: fixed;
-            bottom: 90px;
+            bottom: 100px;
             right: 30px;
             width: 100px;
             height: 100px;
